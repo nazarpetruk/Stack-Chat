f52f41d15b59008fb458641a9a4b785047106361
nazar petruk <nazarpetruk15@gmail.com>
2019-09-03 15:45:05 +0200

    -login UI building

---
+++ Stack Chat/Controller/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  LoginVCViewController.swift
+//  Stack Chat
+//
+//  Created by Nazar Petruk on 03/09/2019.
+//  Copyright © 2019 Nazar Petruk. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Stack Chat/Utillities/Constraints.swift
@@ -0,0 +1,12 @@
+//
+//  Constraints.swift
+//  Stack Chat
+//
+//  Created by Nazar Petruk on 03/09/2019.
+//  Copyright © 2019 Nazar Petruk. All rights reserved.
+//
+
+import Foundation
+
+//MARK: Segues
+let toLogin = "toLogin"
