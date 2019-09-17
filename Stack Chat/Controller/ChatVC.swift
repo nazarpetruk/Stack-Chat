519d7c5637dd743efe76121c708e9437115e1c92
nazar petruk <nazarpetruk15@gmail.com>
2019-09-17 18:04:28 +0200

    - sending message feature - keyboard binding

--- Stack Chat/Controller/ChatVC.swift
+++ Stack Chat/Controller/ChatVC.swift
@@ -11,11 +11,15 @@ import UIKit
 class ChatVC: UIViewController {
     //MARK: IBOutlets
     
+    @IBOutlet weak var messageTxt: UITextField!
     @IBOutlet weak var topLbl: UILabel!
     @IBOutlet weak var menuBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.bindToKeyboard()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapToDismissKeyBoard))
+        view.addGestureRecognizer(tap)
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
@@ -39,10 +43,25 @@ class ChatVC: UIViewController {
     @objc func channelSelected(_ notif: Notification){
         updateWithChannel()
     }
+    @objc func tapToDismissKeyBoard (){
+        view.endEditing(true)
+    }
     func updateWithChannel(){
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         topLbl.text = "#\(channelName)"
         getMessages()
+    }
+    @IBAction func sendMessagePressed(_ sender: Any) {
+        if AuthService.instance.isLogged {
+            guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+            guard let message = messageTxt.text else { return }
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+                if success {
+                    self.messageTxt.text = ""
+                    self.messageTxt.resignFirstResponder()
+                }
+            }
+        }
     }
     
     func onLoginGetMessages(){
