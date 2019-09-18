8f1a230d991e80ee11356689f28df2f069b052b8
nazar petruk <nazarpetruk15@gmail.com>
2019-09-18 13:09:50 +0200

    - adding date formatter

--- Stack Chat/View/MessageCell.swift
+++ Stack Chat/View/MessageCell.swift
@@ -25,5 +25,19 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        //MARK: Data formatting 
+        guard var isoDate = message.timeStamp else { return }
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        let isoFormater = ISO8601DateFormatter()
+        let chatDate = isoFormater.date(from: isoDate.appending("Z"))
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate{
+            let finalDate = newFormatter.string(from: finalDate)
+            timeLbl.text = finalDate
+        }
     }
 }
